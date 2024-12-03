fff2dbc63a33e6bd7218a090890708ebdbcad5c9
Oussama Boufari <oussamaboufari@gmail.com>
2024-12-03 15:33:15 +0100

    "Updated HTML and TypeScript code for movies, profile, signin pages"

--- src/app/pages/signin/sginin.page.ts
+++ src/app/pages/signin/sginin.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sginin',
   templateUrl: './sginin.page.html',
   styleUrls: ['./sginin.page.scss'],
 })
-export class SgininPage implements OnInit {
+export class SgininPage {
+  email = '';
+  password = '';
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  signin() {
+    console.log('Signing in with email:', this.email, 'and password:', this.password);
+    this.authService.login(this.email, this.password).then(() => {
+      console.log('Signed in successfully');
+      window.location.href = '/';
+    }).catch(error => {
+      console.error('Error signing in:', error);
+    });
   }
-
 }
