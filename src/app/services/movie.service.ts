1f49bc19a6c9f1153c63a539a15c8868505b798f
Oussama Boufari <oussamaboufari@gmail.com>
2024-12-03 16:08:01 +0100

    "Updated MovieService: uncommented and corrected addToFavorites method, and reformatted calculateMatch method"

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -5,6 +5,8 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
 import { constants } from '../utils/constants';
+import firebase from 'firebase/compat/app';
+
 
 @Injectable({
   providedIn: 'root',
@@ -25,15 +27,21 @@ export class MovieService {
       `${constants.TMDB_API_URL}/movie/${movieId}?api_key=${constants.TMDB_API_KEY}`
     );
   }
-  // addToFavorites(userId: string, movieId: string) {
-  //   return this.firestore
-  //     .collection('users')
-  //     .doc(userId)
-  //     .update({
-  //       favorites: FieldValue.arrayUnion(movieId), // Add movieId to the favorites.concat(movieId),
-  //     });
-  // }
-  
+  addToFavorites(userId: string, movieId: string): Promise<void> {
+    return this.firestore
+      .collection('users')
+      .doc(userId)
+      .update({
+        favorites: firebase.firestore.FieldValue.arrayUnion(movieId), // Correct usage
+      })
+      .then(() => {
+        console.log('Movie added to favorites successfully!');
+      })
+      .catch((error) => {
+        console.error('Error adding movie to favorites:', error);
+        throw error;
+      });
+  }
 
   // removeFromFavorites(userId: string, movieId: string) {
   //   return this.firestore
@@ -43,30 +51,36 @@ export class MovieService {
   //       favorites: FieldValue.arrayRemove(movieId),
   //     });
   // }
-//   async getMatchingUsers(userId: string): Promise<any[]> {
-//   const currentUser = await this.firestore.collection('users').doc(userId).get();
-//   const currentUserFavorites = currentUser?.data().favorites || [];
 
-//   const allUsers = await this.firestore.collection('users').get().toPromise();
-//   const matchingUsers: any[] | PromiseLike<any[]> = [];
+  //   async getMatchingUsers(userId: string): Promise<any[]> {
+  //   const currentUser = await this.firestore.collection('users').doc(userId).get();
+  //   const currentUserFavorites = currentUser?.data().favorites || [];
 
-//   allUsers.forEach(userDoc => {
-//     const userData = userDoc.data();
-//     if (userData.id !== userId) {
-//       const matchRate = this.calculateMatch(currentUserFavorites, userData.favorites || []);
-//       if (matchRate > 75) {
-//         matchingUsers.push({ ...userData, matchRate });
-//       }
-//     }
-//   });
+  //   const allUsers = await this.firestore.collection('users').get().toPromise();
+  //   const matchingUsers: any[] | PromiseLike<any[]> = [];
 
-//   return matchingUsers;
-// }
+  //   allUsers.forEach(userDoc => {
+  //     const userData = userDoc.data();
+  //     if (userData.id !== userId) {
+  //       const matchRate = this.calculateMatch(currentUserFavorites, userData.favorites || []);
+  //       if (matchRate > 75) {
+  //         matchingUsers.push({ ...userData, matchRate });
+  //       }
+  //     }
+  //   });
 
-calculateMatch(userFavorites: string[], otherUserFavorites: string[]): number {
-  const intersection = userFavorites.filter(movie => otherUserFavorites.includes(movie));
-  const union = new Set([...userFavorites, ...otherUserFavorites]);
-  return (intersection.length / union.size) * 100;
-}
+  //   return matchingUsers;
+  // }
+
+  calculateMatch(
+    userFavorites: string[],
+    otherUserFavorites: string[]
+  ): number {
+    const intersection = userFavorites.filter((movie) =>
+      otherUserFavorites.includes(movie)
+    );
+    const union = new Set([...userFavorites, ...otherUserFavorites]);
+    return (intersection.length / union.size) * 100;
+  }
 
 }
