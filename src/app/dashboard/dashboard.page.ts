f98e537ab6225d98ac1088d7c6f9c5f5e7a98189
Oussama Boufari <oussamaboufari@gmail.com>
2024-12-05 15:41:24 +0100

    update

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { User } from '../models/user';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-
-  constructor() { }
+  users: User[] = [];
+  movieIdToAdd: string = '';
+  selectedUserId: string = '';
+  constructor(
+    private adminService: AdminService,
+    private movieService: MovieService
+  ) {}
 
   ngOnInit() {
+    this.loadUsers();
   }
-
+  loadUsers() {
+    this.adminService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      }
+    });
+  }
+  async addMovieToFavorites() {
+    if (this.selectedUserId && this.movieIdToAdd) {
+      try {
+        await this.adminService.addMovieToUserFavorites(
+          this.selectedUserId,
+          this.movieIdToAdd
+        );
+        console.log('Movie added successfully!');
+      } catch (error) {
+        console.error('Error adding movie to user favorites:', error);
+      }
+    } else {
+      console.warn('User or Movie ID is missing.');
+    }
+  }
 }
